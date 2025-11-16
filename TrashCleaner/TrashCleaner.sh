f514a6ce9a4c749b2dd0e63f93e70541c68d8331
Hau1a <aleksini50@gmail.com>
2025-11-16 16:00:45 +0300

    Add files via upload

---
+++ TrashCleaner/TrashCleaner.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+	echo "ERROR: Скрипт запущен без прав суперпользователя"
+
+else
+
+	#clean file and folder ath the user's request
+	filename="Trash.txt"
+
+	tail -n +4 "$filename" | while read -r path; do
+		if [ -n "$path" ];then
+			if [ -e "$path" ]; then
+				echo "Deleting: $path"
+				sudo rm -rf "$path"
+			else
+				echo "Not exist: $path"
+			fi
+		fi
+	done
+
+	#clean cache
+
+	sudo apt clean
+	sudo journalctl --vacuum-time=7d
+	rm -rf ~/.local/share/Thrash/*
+	rm -rf ~/.cache/*
+	rm -rf ~/.thumbnails/*
+
+fi
